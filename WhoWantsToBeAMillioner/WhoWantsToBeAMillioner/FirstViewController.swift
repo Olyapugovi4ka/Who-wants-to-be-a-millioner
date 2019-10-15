1ec5f68d5b59e48504174b1593933dfe8a198c87
Olyapugovi4ka <olyapugovi4ka@gmail.com>
2019-10-15 14:58:38 +0300

    Add new view controllers

---
+++ WhoWantsToBeAMillioner/WhoWantsToBeAMillioner/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewController.swift
+//  WhoWantsToBeAMillioner
+//
+//  Created by Olga Melnik on 15.10.2019.
+//  Copyright © 2019 Olga Melnik. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
