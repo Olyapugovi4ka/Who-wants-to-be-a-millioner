1bb41a0d81f268324ce8fb1110fd4982f3cabb4e
Olyapugovi4ka <olyapugovi4ka@gmail.com>
2019-10-18 12:01:20 +0300

    delegate

--- WhoWantsToBeAMillioner/WhoWantsToBeAMillioner/Game.swift
+++ WhoWantsToBeAMillioner/WhoWantsToBeAMillioner/Game.swift
@@ -2,11 +2,8 @@
 //  Game.swift
 //  WhoWantsToBeAMillioner
 //
-//  Created by Olga Melnik on 14.10.2019.
+//  Created by Olga Melnik on 18.10.2019.
 //  Copyright © 2019 Olga Melnik. All rights reserved.
 //
 
 import Foundation
-class Game{
-    
-}
