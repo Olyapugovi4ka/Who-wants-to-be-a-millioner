13dff9a41e82da38a19b017498ce4c0b45db55dd
Olyapugovi4ka <olyapugovi4ka@gmail.com>
2019-10-25 21:31:29 +0300

    Saving results

---
+++ WhoWantsToBeAMillioner/WhoWantsToBeAMillioner/GameSession.swift
@@ -0,0 +1,9 @@
+//
+//  GameSession.swift
+//  WhoWantsToBeAMillioner
+//
+//  Created by Olga Melnik on 22.10.2019.
+//  Copyright © 2019 Olga Melnik. All rights reserved.
+//
+
+import Foundation
---
+++ WhoWantsToBeAMillioner/WhoWantsToBeAMillioner/RecordsCaretaker.swift
@@ -0,0 +1,9 @@
+//
+//  RecordsCaretaker.swift
+//  WhoWantsToBeAMillioner
+//
+//  Created by Olga Melnik on 21.10.2019.
+//  Copyright © 2019 Olga Melnik. All rights reserved.
+//
+
+import Foundation
