69456cf9b0f5b281f7c5b0f110a797322c823fe9
Olyapugovi4ka <olyapugovi4ka@gmail.com>
2019-11-01 20:52:21 +0300

    новая логика для добавления вопроса

---
+++ WhoWantsToBeAMillioner/WhoWantsToBeAMillioner/ViewControllers/NewQuestion/Builder/Builder/Catalog/CatalogInteractor.swift
@@ -0,0 +1,9 @@
+//
+//  CatalogInteractor.swift
+//  Builder
+//
+//  Created by Olga Melnik on 01.11.2019.
+//  Copyright © 2019 Olga Melnik. All rights reserved.
+//
+
+import Foundation
---
+++ WhoWantsToBeAMillioner/WhoWantsToBeAMillioner/ViewControllers/NewQuestion/Builder/Builder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Builder
+//
+//  Created by Olga Melnik on 01.11.2019.
+//  Copyright © 2019 Olga Melnik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
